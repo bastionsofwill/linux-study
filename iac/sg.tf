218ffe00bc398365c2d65475a46593eb0d5abbd2
bastionsofwill <bastionsofwill@gmail.com>
2022-10-27 18:45:59 +0900

    fix sg module source

--- iac/sg.tf
+++ iac/sg.tf
@@ -1,5 +1,5 @@
 module "linux-study-sg" {
-  source      = "terraform-aws-modules/security-group/aws/"
+  source      = "terraform-aws-modules/security-group/aws"
   name        = "linux-study-sg"
   description = "Security group for linux-study VPC"
   vpc_id      = module.linux-study-vpc.vpc_id
