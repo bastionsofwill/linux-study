f33b903e3feecdabeaa4356dcea0427e610cb26c
Jayong Hur <jayong.hur@samsung.com>
2023-10-17 20:56:43 +0900

    meaningless change

--- iac/terraform.tf
+++ iac/terraform.tf
@@ -1,16 +1,16 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
   cloud {
     organization = "linux-study"
     hostname     = "app.terraform.io" # Optional; defaults to app.terraform.io
 
     workspaces {
       name = "iac"
-    }
-  }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0"
     }
   }
 }
